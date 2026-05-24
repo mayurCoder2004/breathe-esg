e10857a97ad1d1b9d1805e1a19bb0aff58beac49
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2026-05-24 20:47:53 +0530

    Created api view so that the frontend can upload the files

--- backend/ingestion/views.py
+++ backend/ingestion/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from ingestion.serializers import FileUploadSerializer
+from ingestion.services import CSVIngestionService
+
+
+class FileUploadAPIView(APIView):
+
+    def post(self, request):
+
+        serializer = FileUploadSerializer(
+            data=request.data
+        )
+
+        if serializer.is_valid():
+
+            CSVIngestionService.process_file(
+                company_id=serializer.validated_data["company_id"],
+                source_type=serializer.validated_data["source_type"],
+                uploaded_file=serializer.validated_data["file"]
+            )
+
+            return Response(
+                {"message": "File uploaded successfully"},
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
