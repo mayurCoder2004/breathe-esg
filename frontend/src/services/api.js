664baf260d644adfb36ac91d590f4ea09038ffff
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2026-05-25 12:18:23 +0530

    Completed the whole project

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+﻿import axios from "axios";
+
+const baseURL = (import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api").replace(/\/$/, "");
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: `${baseURL}/`,
 });
 
 export default api;
