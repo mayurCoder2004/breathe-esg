d494a5fd7989ed8e5494283d870a90c598ae21db
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2026-05-25 12:17:45 +0530

    Fix deployment errors

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,7 @@
 ﻿#!/usr/bin/env bash
-set -o errexit
 
-pip install -r backend/requirements.txt
-python backend/manage.py collectstatic --no-input
-python backend/manage.py migrate
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
+python manage.py migrate
