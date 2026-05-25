7a426ca01607a1eed5ad9c25ec99b67b92b3ee2c
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2026-05-25 12:40:04 +0530

    Fixed production company initialization

--- backend/companies/apps.py
+++ backend/companies/apps.py
@@ -2,4 +2,22 @@ from django.apps import AppConfig
 
 
 class CompaniesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'companies'
+
+    def ready(self):
+
+        try:
+            from companies.models import Company
+
+            if not Company.objects.filter(
+                name="Infosys"
+            ).exists():
+
+                Company.objects.create(
+                    name="Infosys",
+                    industry="IT"
+                )
+
+        except Exception:
+            pass
